c1abd712c79d2faf4a047b8781603f1351df5d20
JuanToSeidor <jdtorres@seidoranalytics.com>
2026-05-07 00:36:32 -0300

    debug: add logging to strapi startup

--- cms/server.js
+++ cms/server.js
@@ -1,2 +1,11 @@
 const strapi = require('@strapi/strapi');
-strapi().start();
+
+console.log('--- Iniciando Strapi en Hostinger ---');
+console.log('Directorio actual:', process.cwd());
+console.log('DATABASE_CLIENT:', process.env.DATABASE_CLIENT);
+
+strapi().start().catch(err => {
+  console.error('❌ ERROR CRÍTICO AL ARRANCAR STRAPI:');
+  console.error(err);
+  process.exit(1);
+});
